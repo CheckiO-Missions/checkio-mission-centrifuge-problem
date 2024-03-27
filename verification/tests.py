35f4707c43e9ef1c26d4d9ac95c20b0337d6a40d
Oleksandr Zozulia <freemanlex@gmail.com>
2024-03-27 21:47:58 +0200

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -7,40 +7,24 @@ Each test is a dict with
     "explanation" -- not necessarily a key, it's used for an additional info in animation.
 """
 
-# from random import randint
-# from itertools import chain, product
+from random import randint
 
-# randoms = []
+randoms = []
 
-
-
-# def is_sum(factors, num) -> bool:
-
-#     factors1 = tuple(i for i in factors if i <= num)
-
-#     mult = tuple(num // factor for factor in factors1)
-
-#     return any(sum(a*b for a, b in zip(factors1, comb)) == num
-#                for comb in product(*(range(m + 1) for m in mult)))
+def balanced_centrifuge(n: int, k: int) -> bool:
+    valid = {x for x in range(2, n) if not n % x} | {0, n}
+    for _ in range(n.bit_length()):
+        valid |= {x + y for x in valid for y in valid if x + y < n}
+    return k in valid and n - k in valid
 
 
-# def balanced_centrifuge(n: int, k: int) -> bool:
-
-#     primes = []
-#     for curr in chain([2], range(3, n // 2 + 1, 2), [n]):
-#         if not n % curr and all(curr % j for j in primes):
-#             primes.append(curr)
-
-#     return is_sum(primes, k) and is_sum(primes, n - k)
-
-
-# for _ in range(10):
-#     n = randint(30, 100)
-#     k = randint(1, n)
-#     randoms.append({
-#         "input": [n, k],
-#         "answer": balanced_centrifuge(n, k),
-#     })
+for _ in range(10):
+    n = randint(30, 100)
+    k = randint(1, n)
+    randoms.append({
+        "input": [n, k],
+        "answer": balanced_centrifuge(n, k),
+    })
 
 
 TESTS = {
@@ -68,5 +52,5 @@ TESTS = {
             "answer": False,
         },
     ],
-    # "Random": randoms,
+    "Random": randoms,
 }
